958f6152ac07ed97dee792b37efc98cc49e956ed
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2023-05-02 14:11:33 +0430

    fix skeleton styles

--- src/ui/layouts/BaseLayout.tsx
+++ src/ui/layouts/BaseLayout.tsx
@@ -3,9 +3,11 @@ import Drawer from "../components/drawer/Drawer";
 
 const BaseLayout = () => {
   return (
-    <div className="bg-slate-100 h-screen w-full overflow-hidden">
+    <div className="bg-slate-100 h-screen w-full flex overflow-hidden">
       <Drawer />
-      <Outlet />
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 };
